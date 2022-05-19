8055a947e5d374400269bc524982e6e64959e6ad
Fernando Diaz <fernando.diaz@untoquedigital.com>
2022-05-19 16:16:52 -0500

    Modify user view using the @api_view

--- ecommerce/apps/users/api/api.py
+++ ecommerce/apps/users/api/api.py
@@ -1,14 +1,19 @@
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
+# Al agregar POST me modifica la interfaz para poder agregar un body
+@api_view(['GET', 'POST'])
+def user_api_view(request):
 
-class UserAPIView(APIView):
-    # Retorna un json con todas la info de los users existentes
-    def get(self, request):
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(users_serializer.data)
 
-    
+    # AssertionError at /usuario/usuario/
+    # Expected a `Response`, `HttpResponse` or `HttpStreamingResponse` to be returned from the view, but received a `<class 'NoneType'>`
+    elif request.method == 'POST':
+        print(request.data) 

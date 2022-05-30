6e5d093aeaed6bf8b1647a2e102da6f598666229
Fernando Díaz <feeldiac@gmail.com>
2022-05-30 16:46:07 -0500

    Make custom validations

--- ecommerce/apps/users/api/api.py
+++ ecommerce/apps/users/api/api.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -6,22 +7,27 @@ from apps.users.models import User
 from apps.users.api.serializers import UserSerializer, TestUserSerializer
 
 
-@api_view(['GET', 'POST']) # The api_view decorator takes a list of HTTP methods that your view should respond to. 
+# The api_view decorator takes a list of HTTP methods that your view should respond to.
+@api_view(['GET', 'POST'])
 def user_api_view(request):
     if request.method == 'GET':
         users = User.objects.all()
         user_serializer = UserSerializer(users, many=True)
+
         test_data = {
-            'name': 'develop',
-            'email': 'dev@gmail.com'
+            'name': 'dev',
+            'email': 'developer@gmail.com'
         }
-        test_user = TestUserSerializer(data = test_data)
+
+        test_user = TestUserSerializer(data=test_data, context=test_data)
         if test_user.is_valid():
             print('Pasó validaciones')
+        else:
+            print(test_user.errors)
         return Response(user_serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST': # Al agregar POST me modifica la interfaz para poder agregar un body
-        user_serializer = UserSerializer(data = request.data)
+    elif request.method == 'POST':  # Al agregar POST me modifica la interfaz para poder agregar un body
+        user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
             return Response({'message': 'Usuario creado correctamente.'}, status=status.HTTP_201_CREATED)
@@ -30,7 +36,8 @@ def user_api_view(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_detail_api_view(request, pk=None):
-    user = User.objects.filter(id = pk).first() # Simulamos un get, evitando un try catch
+    # Simulamos un get, evitando un try catch
+    user = User.objects.filter(id=pk).first()
 
     if user:
         if request.method == 'GET':
@@ -38,14 +45,15 @@ def user_detail_api_view(request, pk=None):
             return Response(user_serializer.data, status=status.HTTP_200_OK)
 
         elif request.method == 'PUT':
-            user_serializer = UserSerializer(user, data = request.data) # Instancia + Nueva data
+            user_serializer = UserSerializer(
+                user, data=request.data)  # Instancia + Nueva data
             if user_serializer.is_valid():
                 user_serializer.save()
                 return Response(user_serializer.data, status=status.HTTP_200_OK)
             return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         elif request.method == 'DELETE':
             user.delete()
             return Response({'message': 'Usuario eliminado correctamente.'}, status=status.HTTP_200_OK)
-    
+
     return Response({'message': 'No se ha encontrado un usuario con estos datos'}, status=status.HTTP_400_BAD_REQUEST)
--- ecommerce/apps/users/api/serializers.py
+++ ecommerce/apps/users/api/serializers.py
@@ -1,25 +1,41 @@
 from dataclasses import fields
+from http import server
 from pyexpat import model
 from rest_framework import serializers
 from apps.users.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__' # Todos los campos del modelo serán claves
-        
+        fields = '__all__'  # Todos los campos del modelo serán claves
+
+
 class TestUserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length = 200)
+    name = serializers.CharField(max_length=200)
     email = serializers.EmailField()
 
     def validate_name(self, value):
-        print(value)
+        if 'develop' in value:
+            raise serializers.ValidationError(
+                'Error: no puede existir un usuario con ese nombre')
+            # Siempre retornar el valor para evitar errores
         return value
 
     def validate_email(self, value):
-        print(value)
+        if value == '':
+            raise serializers.ValidationError('Debe suministrar un correo')
+
+        # Esta validacion es asignada al correo⬇
+        if self.validate_name(self.context['name']) in value:
+            raise serializers.ValidationError(
+                'El email no puede contenr el nombre')
         return value
 
     def validate(self, data):
-        print
+        # Dentro de data puedo acceder a todos los campos que se validan en el serializer
+        # Sin embargo desde aquí el error es general y no se asigna a ningún field
+        # if data['name'] in data['email']:
+        #     raise serializers.ValidationError(
+        #         'El email no puede contenr el nombre')
         return data
